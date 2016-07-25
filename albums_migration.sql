3b6e6d41118efb4361d0deed50d83e23875e15c5
Randi Mays <randimays1024@gmail.com>
2016-07-25 14:46:53 -0500

    Adding Albums table for first MySQL table exercise

---
+++ albums_migration.sql
@@ -0,0 +1,11 @@
+USE codeup_test_db;
+DROP TABLE IF EXISTS albums;
+CREATE TABLE albums (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	artist VARCHAR(50) NOT NULL,
+	name VARCHAR(50) NOT NULL,
+	release_date YEAR NOT NULL,
+	sales FLOAT NOT NULL,
+	genre VARCHAR(500) NOT NULL,
+	PRIMARY KEY (id)
+);

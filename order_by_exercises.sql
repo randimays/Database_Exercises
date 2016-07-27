d51159558dc23fd94f3f4af1ff213593a4134c76
Randi Mays <randimays1024@gmail.com>
2016-07-27 13:37:20 -0500

    Updating order by exercises to streamline

---
+++ order_by_exercises.sql
@@ -0,0 +1,5 @@
+SELECT * FROM employees WHERE gender = 'M' AND (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') ORDER BY last_name ASC, first_name ASC;
+SELECT * FROM employees WHERE (last_name LIKE "E%" OR last_name LIKE "%e") ORDER BY emp_no DESC;
+SELECT * FROM employees WHERE (last_name LIKE "E%" AND last_name LIKE "%e") ORDER BY emp_no DESC;
+SELECT * FROM employees WHERE hire_date LIKE '199%' AND birth_date LIKE '%-12-25' ORDER BY hire_date DESC, birth_date ASC;
+SELECT * FROM employees WHERE (last_name LIKE '%q%') AND  (last_name NOT LIKE '%qu%');

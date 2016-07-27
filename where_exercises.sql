863d1542325d5b0634fbe9c69f64273e1284b2b6
Randi Mays <randimays1024@gmail.com>
2016-07-27 11:48:07 -0500

    Adding updated WHERE exercises for 8.7.1 and 8.7.2

---
+++ where_exercises.sql
@@ -0,0 +1,5 @@
+SELECT * FROM employees WHERE gender = 'M' AND (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya');
+SELECT * FROM employees WHERE last_name LIKE "E%" OR last_name LIKE "%e";
+SELECT * FROM employees WHERE last_name LIKE "E%" AND last_name LIKE "%e";
+SELECT * FROM employees WHERE (hire_date BETWEEN '1990-01-01' AND '1999-12-21') AND (birth_date LIKE '%-12-25');
+SELECT * FROM employees WHERE (last_name LIKE '%q%') AND  (last_name !LIKE '%qu%');

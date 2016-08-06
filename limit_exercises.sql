bf076d0f0c7559d4d67bcb3c60428e71fffcbbd0
Randi Mays <randimays1024@gmail.com>
2016-08-06 13:17:07 -0500

    Updating 8.7.5 with exercise descriptions

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -1,2 +1,4 @@
+-- List the first 10 distinct last names sorted in descending order. Find employees born on Christmas & hired in the 90s and update it to just find the first 5. Update the query to find the tenth batch of results.
+
 SELECT DISTINCT last_name FROM employees ORDER BY last_name DESC LIMIT 10;
 SELECT * FROM employees WHERE hire_date LIKE '199%' AND birth_date LIKE '%-12-25' ORDER BY birth_date ASC, hire_date DESC LIMIT 5 OFFSET 45;

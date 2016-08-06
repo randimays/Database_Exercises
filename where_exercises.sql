8ad959c04893064d9dd636ae874c6301d326896f
Randi Mays <randimays1024@gmail.com>
2016-08-06 13:09:29 -0500

    Updating 8.7.1 with exercise descriptions

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,3 +1,5 @@
+-- use Sequel Pro to run queries in your employees database. Find 1) employees with the first names 'Irena', 'Vidya', or 'Maya' 2) employees whose last name starts with 'E' 3) employees hired in the 90s 4) employees born on Christmas 5) employees with a 'q' in their last name. 
+
 SELECT * FROM employees WHERE gender = 'M' AND (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya');
 SELECT * FROM employees WHERE last_name LIKE "E%" OR last_name LIKE "%e";
 SELECT * FROM employees WHERE last_name LIKE "E%" AND last_name LIKE "%e";

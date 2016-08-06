78718ef57f615e6b721b4e6ff9662230717796bf
Randi Mays <randimays1024@gmail.com>
2016-08-06 13:07:39 -0500

    Updating 8.4.4 with exercise descriptions

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,3 +1,5 @@
+-- Write DELETE statements for albums released after 1991, albums with the genre "Progressive Rock", and albums by Nirvana.
+
 USE codeup_test_db;
 
 DELETE FROM albums WHERE release_date > 1990;

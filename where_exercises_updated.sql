65f7c56bb6ffba885385f1b9d23c2b6b8975b37b
Randi Mays <randimays1024@gmail.com>
2016-08-06 13:11:01 -0500

    Updating 8.7.2 with exercise descriptions

--- where_exercises_updated.sql
+++ where_exercises_updated.sql
@@ -1,3 +1,5 @@
+-- Update initial query to use OR instead of IN. Add a condition to find everybody with those names who is male. Find all employees whose last name starts or ends with 'E'. Duplicate the previous query and update it to find all employees whose last name starts AND ends with 'E'. Find all employees hired in the 90s AND born on Christmas. Find all employees with a 'q' in their last name but NOT 'qu'.
+
 SELECT * FROM employees WHERE gender = 'M' AND (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya');
 SELECT * FROM employees WHERE last_name LIKE "E%" OR last_name LIKE "%e";
 SELECT * FROM employees WHERE last_name LIKE "E%" AND last_name LIKE "%e";
